e5b50ad73ecb25c23ef7f6ef7dc4c139fbb13cc5
Dan Rapp <Danvrapp@windstream.net>
2019-07-18 12:56:20 -0400

    Added map & colors

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1Ijoia3VsaW5pIiwiYSI6ImNpeWN6bjJ0NjAwcGYzMnJzOWdoNXNqbnEifQ.jEzGgLAwQnZCv9rA6UTfxQ";
+const API_KEY = "pk.eyJ1IjoiZGFudnJhcHAiLCJhIjoiY2p3ZG1rYnBlMTd4ZDQxcW5hZWRnNHo3NCJ9.E_uilg4-hkWAf19-fqvFPg";
