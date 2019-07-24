51645cb4ad684f052aedb2cec6d363db0c6d2c3e
Anthony Elkadi <aelkadi16@jcu.edu>
2019-07-24 12:00:35 -0400

    adding final files and made changes

--- Final Project - Opioid/Js/maplogic.js
+++ Final Project - Opioid/Js/maplogic.js
@@ -1,8 +1,8 @@
 // Create tile layer
 var backmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  
   maxZoom: 18,
-  id: "mapbox.streets",
+  id: "mapbox.light",
   accessToken: API_KEY
 });
 
@@ -10,6 +10,7 @@ var backmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?
 var p_map = L.map("map1", {
   center: [40.15,  -82.7],
   zoom: 7
+  
 });
 
 //Legend code
